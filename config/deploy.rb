c2e64f2dac4b8acacf0e0f6611b788f8c37d579d
RyuSeungChanne <ryuseungchanne@ryuushousannoMacBook-puro.local>
2018-12-15 15:54:41 +0900

    revise

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
+
 # config valid only for current version of Capistrano
-lock 'capistrano (3.11.0)'
+
+lock '3.11.0'
 
 set :application, 'chat-space2'
 set :repo_url,  'git@github.com:chani84/chat-space2.git'
